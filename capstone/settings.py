44e1a32520bf9fc9cc3e62cdfaa72c173ab429ed
JUNGDAHYE <jungdh618@likelion.org>
2021-02-27 16:14:07 +0900

    second

--- capstone/settings.py
+++ capstone/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,3 +142,9 @@ TAGGIT_LIMIT = 30
 LOGIN_REDIRECT_URL = '/'
 
 CKEDITOR_UPLOAD_PATH = "upload/"
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
